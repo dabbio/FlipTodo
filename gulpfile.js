ca31036226076b863ecd878ecd35fc9fd796342a
dan abbott <dan.abbott@bedegaming.com>
2015-09-29 20:18:16 +0100

    updated package.json and bower.json files and added gulp task

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var minifyCss = require('gulp-minify-css');
+var livereload = require('gulp-livereload');
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch(['./less/**/*.less'], ['less']);
+});
